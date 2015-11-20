f4a3fc6afe93420ca7fe437e01a904bd6f90a9f7
Antonio Pierro <antonio.pierro@gmail.com>
2015-11-20 09:43:55 +0100

    created the first commit of RandomStudent

---
+++ RandomStudent.java
@@ -0,0 +1,9 @@
+public class RandomStudent {
+    public RandomStudent () {
+        System.out.println("HelloWorld");
+    }
+
+    public static void main (String[] args) {
+        RandomStudent randomStudent = new RandomStudent();
+    }
+}
